102f20f2ff4b0c1a1e8d1469b62415715b79300a
MarkGorbunov <markgorbunov1995@mail.ru>
2016-10-03 12:19:15 +0300

    update fo second task

--- homework01/02/src/Greeting.java
+++ homework01/02/src/Greeting.java
@@ -1,5 +1,5 @@
 /**
- * Created by Gorbunov on 02.10.2016.
+ * @author Gorbunov on 02.10.2016.
  * class that write Hello with passed parameter
  */
 public class Greeting {
@@ -7,7 +7,12 @@ public class Greeting {
      * method that write Hello with passed parameter (parameter receive from command line)
      */
     public static void main(String[] args) {
-        int i = 0;
-        System.out.println("Hello " + args[i]);
+        try {
+            int i = 0;
+            System.out.println("Hello " + args[i]);
+        }
+        catch(Exception ex){
+            System.out.println("incorect input");
+        }
     }
 }
