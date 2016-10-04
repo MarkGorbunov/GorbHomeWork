b40ba405013ca4442f22b7f553602c5e2915e463
MarkGorbunov <markgorbunov1995@mail.ru>
2016-10-04 23:42:45 +0300

    update for 4 task

--- homework01/04/src/QuadEquation.java
+++ homework01/04/src/QuadEquation.java
@@ -1,7 +1,7 @@
 import java.util.Scanner;
 
 /**
- * C@author Gorbunov on 04.10.2016.
+ * @author Gorbunov on 04.10.2016.
  *  Class that calculate roots of cuadratic equation
  */
 public class QuadEquation {
@@ -9,54 +9,52 @@ public class QuadEquation {
      * method that calculate roots of cuadratic equation
      */
     public static void main(String[] args) {
+        double[] parameters = new double[3];
+        Scanner scanner = new Scanner(System.in);
         try {
             System.out.println("X^2*a+X*b+c");
-            Scanner in = new Scanner(System.in);
-            System.out.println("input a");
-            double a = in.nextDouble();
-            System.out.println("input b");
-            double b = in.nextDouble();
-            System.out.println("input c");
-            double c = in.nextDouble();
-
-            Linearity(a);
-            if (Discriminant(a, b, c) > 0) { //rooth with Discriminant that > 0
-
-                double x1 = ((-b + Math.sqrt(Discriminant(a, b, c))) / (2 * a));
-                double x2 = ((-b - Math.sqrt(Discriminant(a, b, c))) / (2 * a));
-                System.out.println("roots of equation:" + "x1 = " + x1 + "   x2 = " + x2);
-            } else if (Double.isInfinite(1 / Discriminant(a, b, c))) { //rooth with Discriminant that = 0
-                double x1 = ((-b + Math.sqrt(Discriminant(a, b, c))) / (2 * a));
-                System.out.println("roots of equation:" + "x1 = x2 =" + x1);
-            } else { ///rooth with Discriminant that < 0
-                System.out.println("equation have only imaginary roots");
+            for (int i = 0; i < parameters.length; i++) {
+                System.out.println("Set " + (i + 1) + " parameter");
+                parameters[i] = scanner.nextDouble();
             }
         } catch (Exception ex) {
-            System.out.println("error!! input arguments must be numbers");
+            System.out.println("Incorrect input values. Expected double format");
         }
+        Linearity(parameters);
+        Roots(parameters);
     }
 
     /**
-     * method that return value of descriminant
-     * @param a variable with X^2
-     * @param b variable with X
-     * @param c cariable without X
-     * @return
+     *method that calculate and shows the roots of equation
+     * @param params arguments that are substituted into the equation
      */
-    public static double Discriminant(double a, double b, double c) {
+    public static void Roots(double[] params) {
+        double[] result = new double[2];
         double D;
-        D = (b * b - 4 * a * c);
-        return D;
+        D = (params[1] * params[1] - 4 * params[0] * params[2]);
+        if (D > 0) { //root with Discriminant that > 0
+            result[0] = ((-params[1] + Math.sqrt(D)) / (2 * params[0]));
+            result[1] = ((-params[1] - Math.sqrt(D)) / (2 * params[0]));
+            System.out.println("X1 = " + result[0] + " X2 = " + result[1]);
+        } else if (Double.isInfinite(1 / D)) { //root with Discriminant that = 0
+            result[0] = ((-params[1] + Math.sqrt(D)) / (2 * params[0]));
+            System.out.println("X1 = X2 = " + result[0]);
+        } else { //root with Discriminant that < 0
+            System.out.println("equation have only imaginary roots");
+        }
+
     }
 
     /**
      *method that check equation on lineatity
-     * @param a variable with X^2
+     * @param params argument with X^2
      */
-    public static void Linearity(double a) {
-        if (Double.isInfinite(1 / a)) {
+
+    public static void Linearity(double[] params) {
+        if (Double.isInfinite(1 / params[0])) {
             System.out.println("equation is not quadratic");
             System.exit(0);
         }
     }
 }
+
